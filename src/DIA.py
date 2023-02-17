121b6e79a37531a3f715af636add71514938fa8f
Ivo Roghair <i.roghair@tue.nl>
2023-02-17 09:52:31 +0100

    Add modifications for dedicated experiments

--- src/DIA.py
+++ src/DIA.py
@@ -6,15 +6,15 @@ Hough Circle Transform in order to detect bubbles nucleating/growing
 on a substrate. This still requires a lot of user input in order to
 detect bubbles properly and must cycle between three bubble sizes.
 
-Last modification: 16/02/2023
+Last modification: 08/02/2021
 Authors: Alessandro Battistella, Fabio Mirisola
 """
 
 # Imports
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
 import scipy.io as sio
 
 # DIA functions (must have the corresponding functions.py in the same folder!)
@@ -26,35 +26,35 @@ import DIA_functions as DIA
 
 # File names
 base_folder = '../exp' # Folder that contains all experiments in separate folders
-data_folder = 'sample_image' # Folder containing files to process in subfolder: data
+data_folder = 'default_pore_size_ss_0.04' # Folder containing files to process in subfolder: data
 img_base = 't' # Base name of files to process
 background =  'background_0001.tif' # Background image (path to image)
 output_folder = 'results' # Where to put the results
 
 # Crop parameters
 Crop = True
-Xcoord = 330 
-YCoord = 320
-Width = 1270
-Height = 1270
+Xcoord = 400
+YCoord = 181
+Width = 1370
+Height = 1457
+angle = 5
 
 # Parameters: (for a list of what they do, refer to each of the 
 # functions of opencv/skimage in order to understand)
 
 # parameters large bubbles detection 
-noise_large = {'min_size':1300, 'connectivity':1000}
+noise_large = {'min_size':500, 'connectivity':1000}
 holes_large = {'iterations':4}
-hough_large = {'param1':575, 'param2':16 , 'minRadius':35 , 'maxRadius':60}
+hough_large = {'param1':1000, 'param2':15 , 'minRadius':35 , 'maxRadius':50}
 
 # parameters medium bubbles detection
-noise_medium = {'min_size':920, 'connectivity':1000}
+noise_medium = {'min_size':250, 'connectivity':1000}
 holes_medium = {'iterations':7}
-hough_medium = {'param1':575, 'param2':14 , 'minRadius':17 , 'maxRadius':35}
+hough_medium = {'param1':375, 'param2':13 , 'minRadius':17 , 'maxRadius':35}
 
 # parameters small bubbles detection
-noise_small = {'min_size':320, 'connectivity':10}
-holes_small = {'iterations':7}
-hough_small = {'param1':150, 'param2':17 , 'minRadius':6 , 'maxRadius':16}
+max_radius_small = 25
+min_radius_small = 6
 
 ##################
 # END user input #
@@ -87,12 +87,18 @@ for fname in sorted(filelist):
     # Counter
     n = n + 1
 
+    # Image counter (for detection figures)
+    count = 0
+
     # Print status
     print(f'Analyzing file: {fname}')
 
     # 1) Open Image and Background 
     fname_path = os.path.join(datdir,fname)
     img = DIA.open_image_grayscale(fname_path)
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    DIA.save_image(img,outfile)
+    count += 1
     if background is not None:
         bg_img_path = os.path.join(datdir,background)
         bkg = DIA.open_image_grayscale(bg_img_path)
@@ -103,46 +109,54 @@ for fname in sorted(filelist):
     else:
         process_image = img
 
-    # 3) Crop image
+    # 3) Rotate and crop image
+    process_image = DIA.rotate_image(process_image,angle)
     if Crop is True:
         process_image = process_image[YCoord:YCoord+Height, Xcoord:Xcoord+Width]
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    DIA.save_image(process_image,outfile)
+    count += 1
 
     # Save a copy for later processing
     crop_image = cv.cvtColor(process_image, cv.COLOR_GRAY2BGR)
 
-    # 4) Treshholding
-    process_image = cv.threshold(process_image, 12, 255, cv.THRESH_BINARY)[1]
-
-    # 5) Detect Large (between 35 and 60 pixel radius) bubbles
+    # 4) Detect Large (between 35 and 60 pixel radius) bubbles
     print('Large bubbles\n')
-    circles_large = DIA.detect_bubbles(process_image, noise_large, holes_large, hough_large)
-
-    # 6) Remove large from image
+    circles_large, count = DIA.detect_bubbles(process_image, noise_large, holes_large, hough_large, count, outdir)
+    
+    # 5) Remove large from image
     process_image = DIA.remove_detected_circles(process_image, circles_large)
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    DIA.save_image(process_image,outfile)
+    count += 1
 
-    # 7) Detect Medium (between 17 and 35 pixel radius) bubbles
+    # 6) Detect Medium (between 17 and 35 pixel radius) bubbles
     print('Medium bubbles\n')
-    circles_medium = DIA.detect_bubbles(process_image, noise_medium, holes_medium, hough_medium)
+    circles_medium, count = DIA.detect_bubbles(process_image, noise_medium, holes_medium, hough_medium, count, outdir)
 
-    # 8) Remove medium from image
+    # 7) Remove medium from image
     process_image = DIA.remove_detected_circles(process_image, circles_medium)
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    DIA.save_image(process_image,outfile)
+    count += 1
 
-    # 9) Detect Small (between 6 and 16 pixel radius) bubbles
+    # 8) Detect Small (between 6 and 16 pixel radius) bubbles
     print('Small bubbles\n')
-    circles_small = DIA.detect_bubbles(process_image, noise_small, holes_small, hough_small)
+    circles_small, count = DIA.detect_small(process_image, max_radius_small,min_radius_small,count)
 
-    # 10) Draw all circles on the detection image
+    # 9) Draw all circles on the detection image
     crop_image = DIA.draw_circles(crop_image, circles_large,  (8, 124, 32))
     crop_image = DIA.draw_circles(crop_image, circles_medium, (255, 0, 255))
     crop_image = DIA.draw_circles(crop_image, circles_small,  (0, 0, 255))
 
     # 11) Save image
     output_file = os.path.join(outdir,f'detected{n:05d}.png')
-    # output_file = f'{output_folder}/detected{n:05d}.png'
     DIA.save_image(crop_image,output_file)
 
     # Save data in the dictionary
     Datadict[fname] = DIA.save_dictionary(circles_large,circles_medium,circles_small,fname)
+
+    # DIA.show_image(crop_image)
     
 # Post processing
 #----------------
@@ -152,32 +166,52 @@ for fname in sorted(filelist):
 
 # Plot histograms of bubble sizes
 n = 0       
-max_radius = DIA.max_radius(Datadict)
-bins = np.linspace(0,60,13,dtype=int)
-max_count = 30
-
+max_radius = 60
+bins = np.linspace(0,max_radius,9,dtype=int)
+max_count = 0.15
+Number = np.zeros(len(Datadict))
 for entry in Datadict.keys():
 
     # Counter
     n = n + 1
-    
+
+    # Check if empty
+    if Datadict[entry] is None:
+            continue
+
     # Open a new figure
     plt.figure()
 
     # Make histogram of bubble sizes
-    plt.hist(Datadict[entry][:,2], bins)
+    plt.hist(Datadict[entry][:,2], bins, density=True)
 
     # Make plot nice
     plt.xlabel('Bubble radius [px]')
-    plt.ylabel('Bubble count [-]')
+    plt.ylabel('Probability [-]')
     plt.title(f'Histogram of bubble radii for file {entry}')
     plt.grid(True)
     plt.xlim((0,max_radius))
     plt.ylim((0,max_count))
 
     # Save histogram
-    oup_file = os.path.join(outdir,f'pdf{n:05d}.png')
-    plt.savefig(oup_file,format="png")
+    plt.savefig(f'{output_folder}/pdf{n:05d}.png',format="png")
+
+    # Get number
+    Number[n-1] = Datadict[entry].shape[0]
+
+# Make a plot of (detected) bubble numbers
+time = np.arange(0,len(filelist))
+plt.figure()
+
+plt.plot(time,Number,'o')
+
+plt.xlabel('Time [min]')
+plt.ylabel('Bubble count [-]')
+plt.grid(True)
+plt.ylim((0,300))
+
+outfig = os.path.join(outdir,'bubble_count.png')
+plt.savefig(outfig,format="png")
 
 # Save mat file of analysis
 sio.savemat(os.path.join(outdir,'output.mat'), Datadict)
--- src/DIA_functions.py
+++ src/DIA_functions.py
@@ -1,5 +1,6 @@
 # Module including all DIA functions for nice code separation
 
+import os
 import cv2 as cv
 import numpy as np
 from skimage import morphology
@@ -31,8 +32,31 @@ def show_image(image):
     cv.waitKey(0)
     cv.destroyAllWindows()
 
+# Rotate image
+def rotate_image(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
+  return result
+
 # Image manipulation
 #--------------------
+
+def blur_image(image):
+    """ Blur the image to remove some noise """
+    image = cv.medianBlur(image,7)
+    return image
+
+def threshold(image):
+    """ Do some thresholding operations """
+    # ret,image = cv.threshold(image,150,255,cv.THRESH_BINARY)
+    # image = cv.adaptiveThreshold(image,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+    #         cv.THRESH_BINARY,151,-140)
+    # show_image(image)
+    gamma = 0.125
+    image = adjust_levels(image, gamma)    
+    image = cv.medianBlur(image,5)
+    return image
 
 # Remove small objects (noise)
 def remove_small(image,min_size,connectivity):
@@ -56,6 +80,27 @@ def canny_edge(image):
     image = cv.blur(image,(5,5))
     return feature.canny(image, sigma=3).astype('uint8')*255
 
+def adjust_levels(img, gamma):
+    """ Adjust levels with a given gamma and threshold the resulting image """
+    
+    # TODO might be nice to have levels in input
+    inBlack  = 0
+    inWhite  = 255
+    inGamma  = gamma
+    outBlack = 0
+    outWhite = 255
+
+    # Adjust levels
+    img = np.clip( (img - inBlack) / (inWhite - inBlack), 0, 255 )                            
+    img = ( img ** (1/inGamma) ) *  (outWhite - outBlack) + outBlack
+    img = np.clip( img, 0, 255).astype(np.uint8)
+
+    # Threshold
+    img = cv.threshold(img, 1 , 255, cv.THRESH_BINARY)[1]
+
+    # Return
+    return img
+
 # Hough circle transform
 def hough_transform(image,param1,param2,minRadius,maxRadius):
     """ This function uses cv2 hough circle transform to detect
@@ -73,7 +118,7 @@ def hough_transform(image,param1,param2,minRadius,maxRadius):
     and converted to ints (.astype(int)) """
 
     # Detect circles
-    circles = cv.HoughCircles(image, cv.HOUGH_GRADIENT, 1, minRadius,  
+    circles = cv.HoughCircles(image, cv.HOUGH_GRADIENT, 1, minDist=2*minRadius,  
         param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
 
     # If circles are found, convert to int
@@ -96,16 +141,179 @@ def draw_circles(image,circles,color):
 
 # Remove circles
 def remove_detected_circles(image, circles):
-    """ Draw a black circle over the image, thus actively removing the circles alraedy detected """
+    """ Draw a black circle (+5 pixels radius) over the image, thus actively removing the circles alraedy detected """
     if circles is not None:
         for i in circles:
-            cv.circle(image, (i[0],i[1]), i[2], (0, 0, 0), -1, cv.LINE_AA)  # draw a black circle
+            cv.circle(image, (i[0],i[1]), i[2] + 10, (0, 0, 0), -1, cv.LINE_AA)  # draw a black circle
     return image
 
 # Bubble detection procedure
 #----------------------------
 
-def detect_bubbles(image, noise, holes, hough):
+def get_centroid(contours):
+    """ Get the centroid of a contour using the moments"""
+
+    # Preallocate memory
+    centroids = np.zeros((len(contours),2))
+
+    # Iterate over contours
+    i = -1
+    mask = np.ones(len(centroids),dtype=bool)
+    for contour in contours:
+
+        # Increase counter
+        i = i + 1
+
+        # Find moments
+        M = cv.moments(contour)
+
+        # Find centroid
+        if M['m00'] == 0:   
+            # Entries with no moments (basically unremoved noise) will be removed later
+            centroids[i,0] = 0
+            centroids[i,1] = 0
+            mask[i] = False
+        else:
+            centroids[i,0] = int(M['m10']/M['m00'])
+            centroids[i,1] = int(M['m01']/M['m00'])
+
+    return centroids[mask]
+
+def distance(point_1,point_2):
+    """ Distance between two points (2D) """
+    return np.linalg.norm(point_1 - point_2)
+
+def find_partner(centroids,i,detected,max_radius,min_radius_small):
+    """ Find the partner of i for the circle """
+
+    tolerance = min_radius_small
+    partner = None
+
+    # Check if edge already counted
+    if detected[i] == True:
+        return partner
+
+    # Start loop
+    j = i
+    for c_2 in centroids[i+1:]:
+        
+        # Index
+        j = j + 1
+
+        # Check if already detected
+        if detected[j] == True:
+            continue
+
+        # Calculate distance
+        d = distance(c_2,centroids[i])
+
+        # Check if they are close enough
+        if (d < max_radius*2 and d > min_radius_small*2):
+
+            # Check if they are aligned horizontally
+            if np.abs(centroids[i,1] - c_2[1]) <= tolerance:
+                partner = j
+                break
+
+    return partner
+
+def get_circles_from_pairs(pairs,centroids):
+    """ Get circles from the two contour pairs """
+
+    # Init circles
+    circles = []
+
+    # Loop all over the pairs
+    for p in pairs:
+
+        # Extract centroids
+        c_1 = centroids[p[0]]
+        c_2 = centroids[p[1]]
+
+        # Center of circle
+        cx = (c_1[0] + c_2[0]) / 2
+        cy = (c_1[1] + c_2[1]) / 2
+
+        # Radius of circle  
+        # TODO usually the centroid of the area is about a couple of pixels short of the actual radius, but we should use the contours for distance determination (that's why I add 3 here)
+        r = distance(c_1,c_2)/2 + 3
+
+        # Plug it in circles
+        circles.append([cx, cy, r])
+
+    return np.array(circles)
+
+def get_pairs(contours,max_radius,min_radius_small):
+    """ From contours get pairs of bubble edges and return the pairs """
+
+    # Get centroids
+    centroids = get_centroid(contours)
+
+    # Make a list of pairs
+    pairs = []
+
+    # Sort centroids from left to right
+    centroids = centroids[centroids[:,0].argsort()]
+
+    # Make a mask and start loop
+    detected = np.zeros(len(centroids),dtype=bool)
+    i = -1
+    for c_1 in centroids:
+
+        # Increase counter
+        i = i + 1
+
+        # Detect the possible circle
+        partner = find_partner(centroids,i,detected,max_radius,min_radius_small)
+
+        # Check if something got detected otherwise move on
+        if partner is None:
+            continue
+
+        # Add to the list
+        pairs.append([i,partner])
+
+        # Remove partner from count
+        detected[partner] = True
+
+    return pairs,centroids
+
+def detect_small(image, max_radius,min_radius_small,count):
+    """ Test function for detection of extremely small bubbles """
+
+    # Blur and Threshold
+    blur = blur_image(image)
+    image = threshold(blur)
+    save_image(image,f'results/detection/{count}.png')
+    count += 1
+
+    # Find contours
+    contours, hierarchy = cv.findContours(image, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+
+    # Get pairs from contours
+    pairs,centroids = get_pairs(contours,max_radius,min_radius_small)
+
+    # Print pairs
+    for c in centroids: 
+        cv.circle(image,(c[0].astype(int),c[1].astype(int)), 2, (56, 173, 210), 1, cv.LINE_AA)
+
+    save_image(image,f'results/detection/{count}.png')
+    count += 1
+
+    # Now get circle from pairs
+    circles = get_circles_from_pairs(pairs,centroids).astype(int)
+
+    # Print how many bubbles were found
+    if len(circles) == 0:
+        print('Detected: 0 bubbles\n')
+        return None, count
+    else:
+        print(f'Detected: {circles[:,0].size} bubbles\n')
+
+    # Return detected circles
+    return circles, count
+
+def detect_bubbles(image, noise, holes, hough, count, outdir):
     """ This function takes a thresholded and prepared image and performs a detection of bubbles
     within a specific size. Parameters should be adjusted to the specific size. The procedure consists 
     of a series of steps:
@@ -113,16 +321,35 @@ def detect_bubbles(image, noise, holes, hough):
         2) Close holes
         3) Canny edge
         4) Hough circle transform
+
     """
+
+    # Threshold image
+    image = cv.medianBlur(image,11)
+    image = cv.adaptiveThreshold(image,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,71,-20)
+
+    # Save image after blur
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    save_image(image,outfile)
+    count += 1
 
     # Remove noise
     image = remove_small(image, noise['min_size'], noise['connectivity'])
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    save_image(image,outfile)
+    count += 1
 
     # Close holes
     image = close_holes(image, holes['iterations'])
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    save_image(image,outfile)
+    count += 1
 
     # Canny edge
     image = canny_edge(image)
+    outfile = os.path.join(outdir,'detection',f'{count}.png')
+    save_image(image,outfile)
+    count += 1
 
     # Detect circles
     circles = hough_transform(image, hough['param1'], hough['param2'], hough['minRadius'], hough['maxRadius'])
@@ -133,20 +360,20 @@ def detect_bubbles(image, noise, holes, hough):
     else:
         print('Detected: 0 bubbles\n')
 
-    return circles
+    return circles, count
 
 def save_dictionary(circles_large,circles_medium,circles_small, fname):
     """ Function to check if data is empty and store it into a dictionary """
 
     # Get the array
-    circles = np.concatenate([circles for circles in (circles_small, circles_medium, circles_large) if circles is not None])
+    circles = np.array([circles for circles in (circles_small, circles_medium, circles_large) if circles is not None], dtype=object)
     
     # Check if empty
     if circles.size == 0:
         circles = None
         print(f'Nothing detected for file {fname}')
-    # else:
-    #     circles = np.concatenate(circles)
+    else:
+        circles = np.concatenate(circles)
 
     return circles
 
@@ -157,9 +384,12 @@ def max_radius(DataDictionary):
     """ Find max radius in the dictionary """
     max = 0
     for key in DataDictionary.keys():
-        local_max = np.amax(DataDictionary[key][:,2])
-        if max < local_max:
-            max = local_max
-    
+        if DataDictionary[key] is None:
+            continue
+        else:
+            local_max = np.amax(DataDictionary[key][:,2])
+            if max < local_max:
+                max = local_max
+        
     return max
         
